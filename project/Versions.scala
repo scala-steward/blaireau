56da449e9cbc3c152bc8cfd4c7452f1f88a92b3f
Scala Steward <scala_steward@virtuslab.com>
2024-05-19 20:43:28 +0000

    Update shapeless to 2.3.12

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
   val skunk     = "0.6.2"
-  val shapeless = "2.3.10"
+  val shapeless = "2.3.12"
   val newtype   = "0.4.4"
   val refined   = "0.9.29"
   val munit     = "0.7.29"
